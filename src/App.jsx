53b7b3c9d37f3db139ece5ff4018bf17c12ea3ed
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2024-06-02 12:54:18 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,41 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Navbar color="light" light>
+      <Navbar className="headername" color="light" light>
         <NavbarBrand href="/">Tufel Panarwala</NavbarBrand>
       </Navbar>
-      <div className="container-fluid">Website coming soon</div>
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-column">
+            <h3>Panarwala & Associates</h3>
+            <p>
+              A422, Sun westbank, Nr. Shiv cinema, Ashram road, Ahmedabad,
+              380009
+            </p>
+            <p>&copy; 2024 Company Name. All rights reserved.</p>
+          </div>
+          <div className="footer-column">
+            <h3>Contact Us</h3>
+            <ul>
+              <li>Email: tufel.kcg@gmail.com</li>
+              <li>Mobile: 9974936751</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3>Follow Us</h3>
+            <div className="social-icons">
+              <a
+                href="https://www.facebook.com/profile.php?id=61555952280720"
+                className="fa fa-facebook"
+              ></a>
+              <a
+                href="https://www.instagram.com/panarwala_associates/"
+                className="fa fa-instagram"
+              ></a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
